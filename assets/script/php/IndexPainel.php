14d1d4816f2dfa6c181c4a2f5b4b55e23f6897b6
Matheus Santos Da Fonseca <matheus.fonseca@picpay.com>
2022-10-23 13:54:56 -0300

    Refatoração

--- assets/script/php/IndexPainel.php
+++ assets/script/php/IndexPainel.php
@@ -9,26 +9,26 @@ if (isset($_POST['add'])) {
   $add = new AddTarefa();
   $add->addTarefa($_POST['tarefa'], $_POST['username']);
 
-  header("Location: http://localhost/crudphp/painel.php");
+  header("Location: painel.php");
 }
 
 if (isset($_POST['concluir'])) {
   $concluir = new ConcluirTarefa();
   $concluir->concluirTarefa($_POST['tarefa']);
 
-  header("Location: http://localhost/crudphp/painel.php");
+  header("Location: painel.php");
 }
 
 if (isset($_POST['editar'])) {
   $e = new EditarTarefa();
   $e->editarTarefa($_POST['tarefa'], $_POST['idTarefa']);
 
-  header("Location: http://localhost/crudphp/painel.php");
+  header("Location: painel.php");
 }
 
 if (isset($_POST['excluir'])) {
   $ex = new ExcluirTarefa();
   $ex->excluirTarefa($_POST['tarefa']);
 
-  header("Location: http://localhost/crudphp/painel.php");
+  header("Location: painel.php");
 }
