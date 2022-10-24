9904075aa4d160ca6d43578b7d5523c51a5bded7
Matheus Santos Da Fonseca <matheus.fonseca@picpay.com>
2022-10-24 14:53:52 -0300

    Deixando uma mensagem para caso não existir sessão ativa

--- assets/script/php/verificaLogin.php
+++ assets/script/php/verificaLogin.php
@@ -2,6 +2,6 @@
 <?php
 // se não existe uma sessão chamada 'usuario' será redirecionada para a pagina de login
 if (!$_SESSION['Username']) {
-  header("Location: index.php");
+  header('Location: https://crud-php-dev-matheussantos.herokuapp.com/index.php?msgErro=Faça seu Login');
   exit();
 }
